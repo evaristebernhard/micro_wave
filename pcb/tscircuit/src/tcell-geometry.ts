ac68f319bf0fa0d6f43c11c493c32f49b572b9f8
evaristebernhardwiener@gmai.com <evaristebernhardwiener@gmail.com>
2026-09-18 22:47:46 +0800

    pcb: lock T-cell geometry to inner-pad reference model

--- pcb/tscircuit/src/tcell-geometry.ts
+++ pcb/tscircuit/src/tcell-geometry.ts
@@ -1,15 +1,15 @@
 import { matchedExtractionTCellSeed, type BoardClass } from "./geometry"
 
 export const tCellRfPolygonPoints = {
-  A: [{ x: -7.521, y: -26.569 },{ x: -6.967, y: -26.799 },{ x: -6.659, y: -26.671 },{ x: -6.582, y: -26.974 },{ x: 22.885, y: -19.521 },{ x: 22.115, y: -16.479 },{ x: -5.651, y: -23.502 },{ x: 0.047, y: -10.25 },{ x: 0.721, y: -9.971 },{ x: 1.02, y: -9.25 },{ x: 1.569, y: -9.25 },{ x: 1.569, y: 1.45 },{ x: -1.569, y: 1.45 },{ x: -1.569, y: -9.25 },{ x: -1.02, y: -9.25 },{ x: -0.753, y: -9.892 },{ x: -6.6, y: -23.489 },{ x: -21.146, y: -16.278 },{ x: -22, y: -18 },{ x: -22, y: -16.431 },{ x: -22.5, y: -16.431 },{ x: -22.5, y: -19.009 },{ x: -22.854, y: -19.722 },{ x: -7.821, y: -27.175 }],
-  B: [{ x: -7.51, y: -26.434 },{ x: -6.919, y: -26.678 },{ x: -6.59, y: -26.542 },{ x: -6.546, y: -26.72 },{ x: 22.873, y: -19.524 },{ x: 22.127, y: -16.476 },{ x: -5.208, y: -23.163 },{ x: 0.471, y: -10.074 },{ x: 0.721, y: -9.971 },{ x: 1.02, y: -9.25 },{ x: 1.569, y: -9.25 },{ x: 1.569, y: 1.45 },{ x: -1.569, y: 1.45 },{ x: -1.569, y: -9.25 },{ x: -1.02, y: -9.25 },{ x: -0.926, y: -9.475 },{ x: -6.762, y: -22.925 },{ x: -21.088, y: -16.089 },{ x: -22, y: -18 },{ x: -22, y: -16.431 },{ x: -22.5, y: -16.431 },{ x: -22.5, y: -19.048 },{ x: -22.912, y: -19.911 },{ x: -7.831, y: -27.107 }],
-  C: [{ x: -6.878, y: -26.576 },{ x: -5.551, y: -26.014 },{ x: 22.849, y: -19.529 },{ x: 22.151, y: -16.471 },{ x: -4.164, y: -22.479 },{ x: 1.438, y: -9.89 },{ x: 0.864, y: -9.634 },{ x: 1.023, y: -9.25 },{ x: 1.569, y: -9.25 },{ x: 1.569, y: 1.45 },{ x: -1.569, y: 1.45 },{ x: -1.569, y: -8.904 },{ x: -7.232, y: -21.632 },{ x: -20.918, y: -15.561 },{ x: -22, y: -18 },{ x: -22, y: -16.431 },{ x: -22.5, y: -16.431 },{ x: -22.5, y: -19.127 },{ x: -23.082, y: -20.439 },{ x: -7.96, y: -27.147 },{ x: -7.578, y: -26.286 }],
-  D: [{ x: -7.749, y: -22.628 },{ x: -21.881, y: -16.559 },{ x: -23.119, y: -19.441 },{ x: -6.155, y: -26.726 },{ x: 1.569, y: -9.587 },{ x: 1.569, y: 1.45 },{ x: -1.569, y: 1.45 },{ x: -1.569, y: -8.913 }]
+  A: [{ x: -6.098, y: -27.863 },{ x: -6.491, y: -27.985 },{ x: -6.618, y: -27.580 },{ x: -21.119, y: -19.689 },{ x: -22.807, y: -18.770 },{ x: -20.970, y: -15.393 },{ x: -5.248, y: -23.948 },{ x: -0.933, y: -10.700 },{ x: -1.450, y: -10.700 },{ x: -1.450, y: 1.450 },{ x: -1.450, y: 2.900 },{ x: 1.450, y: 2.900 },{ x: 1.450, y: -9.250 },{ x: 1.450, y: -10.700 },{ x: -0.011, y: -10.700 },{ x: -4.353, y: -24.030 },{ x: 19.732, y: -16.503 },{ x: 21.229, y: -16.035 },{ x: 22.165, y: -19.029 },{ x: -3.902, y: -27.176 },{ x: -4.692, y: -28.628 }],
+  B: [{ x: -6.315, y: -27.685 },{ x: -6.452, y: -27.727 },{ x: -6.493, y: -27.592 },{ x: -21.185, y: -19.875 },{ x: -23.060, y: -18.890 },{ x: -21.090, y: -15.140 },{ x: -5.421, y: -23.370 },{ x: -1.275, y: -10.700 },{ x: -1.450, y: -10.700 },{ x: -1.450, y: 1.450 },{ x: -1.450, y: 2.900 },{ x: 1.450, y: 2.900 },{ x: 1.450, y: -9.250 },{ x: 1.450, y: -10.700 },{ x: 0.326, y: -10.700 },{ x: -3.922, y: -23.681 },{ x: 19.745, y: -16.499 },{ x: 21.245, y: -16.044 },{ x: 22.156, y: -19.045 },{ x: -3.585, y: -26.857 },{ x: -4.516, y: -28.630 }],
+  C: [{ x: 19.769, y: -16.492 },{ x: 21.277, y: -16.061 },{ x: 22.139, y: -19.077 },{ x: -2.828, y: -26.209 },{ x: -4.137, y: -28.871 },{ x: -21.377, y: -20.395 },{ x: -23.772, y: -19.217 },{ x: -21.417, y: -14.428 },{ x: -5.927, y: -22.044 },{ x: -1.493, y: -8.752 },{ x: -1.450, y: -8.624 },{ x: -1.450, y: 1.450 },{ x: -1.450, y: 2.900 },{ x: 1.450, y: 2.900 },{ x: 1.450, y: -8.075 },{ x: 1.991, y: -8.255 },{ x: 1.450, y: -9.876 },{ x: 1.450, y: -10.700 },{ x: 1.175, y: -10.700 },{ x: -2.919, y: -22.973 }],
+  D: [{ x: -1.499, y: -8.787 },{ x: -1.450, y: -8.630 },{ x: -1.450, y: 1.450 },{ x: -1.450, y: 2.900 },{ x: 1.450, y: 2.900 },{ x: 1.450, y: -8.056 },{ x: 1.961, y: -8.214 },{ x: 1.450, y: -9.870 },{ x: 1.450, y: -10.700 },{ x: 1.194, y: -10.700 },{ x: -4.033, y: -27.631 },{ x: -20.888, y: -19.410 },{ x: -22.297, y: -18.722 },{ x: -20.922, y: -15.903 },{ x: -5.950, y: -23.206 }]
 } as const
 
-const bridgeResidual = (boardClass: "A" | "B" | "C") =>
-  matchedExtractionTCellSeed.variants[boardClass].requiredBridgePhaseDeg -
-  matchedExtractionTCellSeed.commonBridgePhaseSeedDeg
+const intercellResidual = (boardClass: "A" | "B" | "C") =>
+  matchedExtractionTCellSeed.variants[boardClass].requiredIntercellPhaseDeg -
+  matchedExtractionTCellSeed.commonIntercellPhaseSeedDeg
 
 export const tCellVariantMeta = {
   A: {
@@ -17,12 +17,10 @@ export const tCellVariantMeta = {
     k: matchedExtractionTCellSeed.variants.A.k,
     junctionX: matchedExtractionTCellSeed.variants.A.junctionX,
     junctionY: matchedExtractionTCellSeed.variants.A.junctionY,
-    seriesTransformerOhm:
-      matchedExtractionTCellSeed.variants.A.seriesTransformerOhm,
-    branchTransformerOhm:
-      matchedExtractionTCellSeed.variants.A.branchTransformerOhm,
-    bridgePhaseDeg: matchedExtractionTCellSeed.variants.A.requiredBridgePhaseDeg,
-    bridgeResidualPhaseDeg: bridgeResidual("A"),
+    seriesTransformerOhm: matchedExtractionTCellSeed.variants.A.seriesTransformerOhm,
+    branchTransformerOhm: matchedExtractionTCellSeed.variants.A.branchTransformerOhm,
+    bridgePhaseDeg: matchedExtractionTCellSeed.variants.A.requiredIntercellPhaseDeg,
+    bridgeResidualPhaseDeg: intercellResidual("A"),
     hasRfOut: true
   },
   B: {
@@ -30,12 +28,10 @@ export const tCellVariantMeta = {
     k: matchedExtractionTCellSeed.variants.B.k,
     junctionX: matchedExtractionTCellSeed.variants.B.junctionX,
     junctionY: matchedExtractionTCellSeed.variants.B.junctionY,
-    seriesTransformerOhm:
-      matchedExtractionTCellSeed.variants.B.seriesTransformerOhm,
-    branchTransformerOhm:
-      matchedExtractionTCellSeed.variants.B.branchTransformerOhm,
-    bridgePhaseDeg: matchedExtractionTCellSeed.variants.B.requiredBridgePhaseDeg,
-    bridgeResidualPhaseDeg: bridgeResidual("B"),
+    seriesTransformerOhm: matchedExtractionTCellSeed.variants.B.seriesTransformerOhm,
+    branchTransformerOhm: matchedExtractionTCellSeed.variants.B.branchTransformerOhm,
+    bridgePhaseDeg: matchedExtractionTCellSeed.variants.B.requiredIntercellPhaseDeg,
+    bridgeResidualPhaseDeg: intercellResidual("B"),
     hasRfOut: true
   },
   C: {
@@ -43,12 +39,10 @@ export const tCellVariantMeta = {
     k: matchedExtractionTCellSeed.variants.C.k,
     junctionX: matchedExtractionTCellSeed.variants.C.junctionX,
     junctionY: matchedExtractionTCellSeed.variants.C.junctionY,
-    seriesTransformerOhm:
-      matchedExtractionTCellSeed.variants.C.seriesTransformerOhm,
-    branchTransformerOhm:
-      matchedExtractionTCellSeed.variants.C.branchTransformerOhm,
-    bridgePhaseDeg: matchedExtractionTCellSeed.variants.C.requiredBridgePhaseDeg,
-    bridgeResidualPhaseDeg: bridgeResidual("C"),
+    seriesTransformerOhm: matchedExtractionTCellSeed.variants.C.seriesTransformerOhm,
+    branchTransformerOhm: matchedExtractionTCellSeed.variants.C.branchTransformerOhm,
+    bridgePhaseDeg: matchedExtractionTCellSeed.variants.C.requiredIntercellPhaseDeg,
+    bridgeResidualPhaseDeg: intercellResidual("C"),
     hasRfOut: true
   },
   D: {
